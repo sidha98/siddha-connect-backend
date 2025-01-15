dfd34b7b7dd93740460b9c410770b859fd1de5ae
Rakshita Kanwar <rakshitakanwar09@gmail.com>
2025-01-15 17:08:40 +0530

    tall trans rec

--- routes/tallyTransactionRoutes.js
+++ routes/tallyTransactionRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../services/fileUpload");
-const { uploadTallyTransactions, getTallyTransactionForDealer } = require("../controllers/tallyTransactionControllers");
+const { uploadTallyTransactions, getTallyTransactionForDealer, uploadTallyTransactionsThroughMainMachine } = require("../controllers/tallyTransactionControllers");
 const { dealerAuth } = require("../middlewares/authMiddlewares");
 
 router.post("/upload-tally-transactions", upload.single("file"), uploadTallyTransactions);
 router.get("/get-tally-transactions-for-dealer", dealerAuth, getTallyTransactionForDealer);
+router.post("/upload-tally-transactions-through-main-machine", uploadTallyTransactionsThroughMainMachine);
 
 module.exports = router;
